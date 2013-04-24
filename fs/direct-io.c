44e3f83d1ae6f109827d7e1718de073098fad8ee
Jens Axboe <axboe@kernel.dk>
2013-04-24 15:42:09 -0600

    direct-io: only inc/dec inode->i_dio_count for file systems
    We don't need truncate protection for block devices, so add a flag
    bypassing this cache line dirtying twice for every IO. This easily
    contributes to 5-10% of the CPU time on high IOPS O_DIRECT testing.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/direct-io.c
+++ fs/direct-io.c
@@ -265,7 +265,11 @@ static ssize_t dio_complete(struct dio *dio, loff_t offset, ssize_t ret,
 	if (dio->end_io && dio->result)
 		dio->end_io(dio->iocb, offset, transferred, dio->private);
 
-	inode_dio_done(dio->inode);
+	if (dio->flags & DIO_IGNORE_TRUNCATE)
+		__inode_dio_done(dio->inode);
+	else
+		inode_dio_done(dio->inode);
+
 	if (is_async) {
 		if (dio->rw & WRITE) {
 			int err;
@@ -1194,6 +1198,17 @@ do_blockdev_direct_IO(int rw, struct kiocb *iocb, struct inode *inode,
 	}
 
 	/*
+<<<<<<< HEAD
+=======
+	 * Will be decremented at I/O completion time. For a block device
+	 * we don't need to protect against truncate, so don't increment
+	 * the inode direct IO count.
+	 */
+	if (!(dio->flags & DIO_IGNORE_TRUNCATE))
+		atomic_inc(&inode->i_dio_count);
+
+	/*
+>>>>>>> direct-io: only inc/dec inode->i_dio_count for file systems
 	 * For file extending writes updating i_size before data
 	 * writeouts complete can expose uninitialized blocks. So
 	 * even for AIO, we need to wait for i/o to complete before
