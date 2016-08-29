0afd4f6e8c2b1b4aa109b87940d9db466b60b53e
Alexander Gordeev <agordeev@redhat.com>
2016-08-29 12:42:32 +0200

    blk-mq: Fix memory leaks on a queue cleanup
    Some data are leaked when blk_cleanup_queue() interface
    is called.
    
    CC: Jens Axboe <axboe@kernel.dk>
    CC: linux-nvme@lists.infradead.org
    Signed-off-by: Alexander Gordeev <agordeev@redhat.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1707,8 +1707,13 @@ static void blk_mq_free_hw_queues(struct request_queue *q,
 	struct blk_mq_hw_ctx *hctx;
 	unsigned int i;
 
-	queue_for_each_hw_ctx(q, hctx, i)
+	queue_for_each_hw_ctx(q, hctx, i) {
 		free_cpumask_var(hctx->cpumask);
+		kfree(hctx->ctxs);
+		kfree(hctx);
+	}
+
+	q->nr_hw_queues = 0;
 }
 
 static int blk_mq_init_hctx(struct request_queue *q,
