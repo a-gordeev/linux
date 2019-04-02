15ffbe725b8630406c75e4e28516f7250919dba0
Alexander Gordeev <alexander.gordeev@daqri.com>
2019-04-02 08:43:07 +0200

    staging: avalon-dma: Rework PCI initialization
    Signed-off-by: Alexander Gordeev <alexander.gordeev@daqri.com>

--- drivers/staging/avalon-dma/avalon-drv-core.c
+++ drivers/staging/avalon-dma/avalon-drv-core.c
@@ -9,64 +9,101 @@
 
 #define PCI_VENDOR_ID_ALARIC	0x1172
 
-static int __init avalon_pci_probe(struct pci_dev *dev,
+static int avalon_dev_register(struct avalon_dev *avalon_dev,
+			       const struct file_operations *fops)
+{
+	avalon_dev->misc_dev.minor	= MISC_DYNAMIC_MINOR;
+	avalon_dev->misc_dev.name	= DRIVER_NAME;
+	avalon_dev->misc_dev.nodename	= DRIVER_NAME;
+	avalon_dev->misc_dev.fops	= fops;
+	avalon_dev->misc_dev.mode	= 0644;
+
+	return misc_register(&avalon_dev->misc_dev);
+}
+
+static void avalon_dev_unregister(struct avalon_dev *avalon_dev)
+{
+	misc_deregister(&avalon_dev->misc_dev);
+}
+
+static int __init avalon_pci_probe(struct pci_dev *pci_dev,
 				   const struct pci_device_id *id)
 {
 	struct avalon_dev *avalon_dev;
-	int rc;
+	int ret;
 
 	avalon_dev = kzalloc(sizeof(*avalon_dev), GFP_KERNEL);
-	BUG_ON(!avalon_dev);
+	if (!avalon_dev)
+		return -ENOMEM;
 
-	avalon_dev->pci_dev = dev;
-	pci_set_drvdata(dev, avalon_dev);
+	ret = pci_enable_device(pci_dev);
+	if (ret)
+		goto enable_err;
 
-	avalon_dev->misc_dev.minor = MISC_DYNAMIC_MINOR;
-	avalon_dev->misc_dev.name = DRIVER_NAME;
-	avalon_dev->misc_dev.nodename = DRIVER_NAME;
-	avalon_dev->misc_dev.fops = &avalon_dev_fops;
-	avalon_dev->misc_dev.mode = 0666;
+	ret = pci_request_regions(pci_dev, DRIVER_NAME);
+	if (ret)
+		goto reg_err;
 
-	rc = misc_register(&avalon_dev->misc_dev);
-	BUG_ON(rc);
+	ret = init_interrupts(avalon_dev);
+	if (ret)
+		goto int_err;
 
-	rc = pci_enable_device(dev);
-	BUG_ON(rc);
+	ret = avalon_dma_init(&avalon_dev->avalon_dma, pci_dev);
+	if (ret)
+		goto dma_err;
 
-	rc = pci_request_regions(dev, DRIVER_NAME);
-	BUG_ON(rc);
+	ret = avalon_dev_register(avalon_dev, &avalon_dev_fops);
+	if (ret)
+		goto dev_reg_err;
 
-	pci_set_master(dev);
+	ret = init_attributes(&pci_dev->dev);
+	if (ret)
+		goto attr_err;
 
-	pci_write_config_byte(dev, PCI_INTERRUPT_LINE, dev->irq);
+	pci_set_master(pci_dev);
+	pci_write_config_byte(pci_dev, PCI_INTERRUPT_LINE, pci_dev->irq);
 
-	rc = init_interrupts(avalon_dev);
-	BUG_ON(rc);
-
-	rc = avalon_dma_init(&avalon_dev->avalon_dma, dev);
-	BUG_ON(rc);
-
-	rc = init_attributes(&dev->dev);
-	BUG_ON(rc);
+	avalon_dev->pci_dev = pci_dev;
+	pci_set_drvdata(pci_dev, avalon_dev);
 
 	return 0;
-}
 
-static void __exit avalon_pci_remove(struct pci_dev *dev)
-{
-	struct avalon_dev *avalon_dev = pci_get_drvdata(dev);
+attr_err:
+	avalon_dev_unregister(avalon_dev);
 
-	term_attributes(&dev->dev);
+dev_reg_err:
+	avalon_dma_term(&avalon_dev->avalon_dma);
 
-	avalon_dev = pci_get_drvdata(dev);
+dma_err:
 	term_interrupts(avalon_dev);
 
-	misc_deregister(&avalon_dev->misc_dev);
+int_err:
+	pci_release_regions(pci_dev);
 
-	pci_disable_device(dev);
-	pci_release_regions(dev);
+reg_err:
+	pci_disable_device(pci_dev);
+
+enable_err:
+	kfree(avalon_dev);
+
+	return ret;
+}
+
+static void __exit avalon_pci_remove(struct pci_dev *pci_dev)
+{
+	struct avalon_dev *avalon_dev = pci_get_drvdata(pci_dev);
+
+	pci_set_drvdata(pci_dev, NULL);
+
+	term_attributes(&pci_dev->dev);
+	avalon_dev_unregister(avalon_dev);
 
 	avalon_dma_term(&avalon_dev->avalon_dma);
+
+	term_interrupts(avalon_dev);
+
+	pci_release_regions(pci_dev);
+	pci_disable_device(pci_dev);
 
 	kfree(avalon_dev);
 }
