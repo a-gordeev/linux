477de832db20c9d3dd4e57926dcac22196e192cc
Alexander Gordeev <alexander.gordeev@daqri.com>
2019-04-02 11:32:08 +0200

    staging: avalon-dma: Do not create attributes if AVALON_DEBUG_STATS is not set
    Signed-off-by: Alexander Gordeev <alexander.gordeev@daqri.com>

--- drivers/staging/avalon-dma/avalon-drv-attr.c
+++ drivers/staging/avalon-dma/avalon-drv-attr.c
@@ -2,29 +2,22 @@
 
 #include "avalon-drv.h"
 
+#ifdef AVALON_DEBUG_STATS
 static ssize_t avalon_dma_stats_show(struct device *dev,
 				     struct device_attribute *attr, char *buf)
 {
-#ifdef AVALON_DEBUG_STATS
 	struct avalon_dev *avalon_dev = dev_get_drvdata(dev);
 
         return avalon_dma_print_stats(buf, PAGE_SIZE, &avalon_dev->avalon_dma);
-#else
-	return -ENOSYS;
-#endif
 }
 static DEVICE_ATTR_RO(avalon_dma_stats);
 
 static ssize_t avalon_dma_lists_show(struct device *dev,
 				     struct device_attribute *attr, char *buf)
 {
-#ifdef AVALON_DEBUG_STATS
 	struct avalon_dev *avalon_dev = dev_get_drvdata(dev);
 
         return avalon_dma_print_lists(buf, PAGE_SIZE, &avalon_dev->avalon_dma);
-#else
-	return -ENOSYS;
-#endif
 }
 static DEVICE_ATTR_RO(avalon_dma_lists);
 
@@ -41,3 +34,13 @@ void term_attributes(struct device *dev)
 	device_remove_file(dev, &dev_attr_avalon_dma_lists);
 	device_remove_file(dev, &dev_attr_avalon_dma_stats);
 }
+#else
+int init_attributes(struct device *dev)
+{
+	return 0;
+}
+
+void term_attributes(struct device *dev)
+{
+}
+#endif
