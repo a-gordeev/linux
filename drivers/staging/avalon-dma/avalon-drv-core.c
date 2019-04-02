6c0855678650b8b5f378e9fb085aaaf2a1180688
Alexander Gordeev <alexander.gordeev@daqri.com>
2019-04-02 20:44:55 +0200

    staging: avalon-dma: Move interrupt handling to avalon-drv-core.c
    Signed-off-by: Alexander Gordeev <alexander.gordeev@daqri.com>

--- drivers/staging/avalon-dma/avalon-drv-core.c
+++ drivers/staging/avalon-dma/avalon-drv-core.c
@@ -4,11 +4,58 @@
 
 #include "avalon-drv.h"
 #include "avalon-drv-fops.h"
-#include "avalon-drv-interrupt.h"
 #include "avalon-drv-attr.h"
 
 #define PCI_VENDOR_ID_ALARIC	0x1172
 
+#define DMA_STATUS_INT		0
+#define AVALON_MSI_COUNT	4
+
+static irqreturn_t dma_interrupt(int irq, void *dev_id)
+{
+	struct avalon_dev *avalon_dev = (struct avalon_dev*)dev_id;
+
+	return avalon_dma_interrupt(&avalon_dev->avalon_dma);
+}
+
+static int init_interrupts(struct avalon_dev *avalon_dev,
+			   struct pci_dev *pci_dev)
+{
+	int vec;
+	int ret;
+
+	ret = pci_alloc_irq_vectors(pci_dev,
+				    AVALON_MSI_COUNT, AVALON_MSI_COUNT,
+				    PCI_IRQ_MSI);
+	if (ret < 0)
+		goto msi_err;
+	else if (ret != AVALON_MSI_COUNT)
+		goto nr_msi_err;
+
+	vec = pci_irq_vector(pci_dev, DMA_STATUS_INT);
+	ret = request_irq(vec, dma_interrupt, IRQF_SHARED,
+			  DRIVER_NAME, avalon_dev);
+	if (ret)
+		goto req_irq_err;
+
+	return 0;
+
+req_irq_err:
+nr_msi_err:
+	pci_disable_msi(pci_dev);
+
+msi_err:
+	return ret;
+}
+
+static void term_interrupts(struct avalon_dev *avalon_dev)
+{
+	struct pci_dev *pci_dev = avalon_dev->pci_dev;
+	int vec = pci_irq_vector(pci_dev, DMA_STATUS_INT);
+
+	free_irq(vec, (void*)avalon_dev);
+	pci_disable_msi(pci_dev);
+}
 static int avalon_dev_register(struct avalon_dev *avalon_dev,
 			       const struct file_operations *fops)
 {
--- drivers/staging/avalon-dma/avalon-drv-interrupt.h
+++
@@ -1,7 +0,0 @@
-#ifndef __AVALON_DRV_INTERRUPT_H__
-#define __AVALON_DRV_INTERRUPT_H__
-
-int init_interrupts(struct avalon_dev *avalon_dev, struct pci_dev *pci_dev);
-void term_interrupts(struct avalon_dev *avalon_dev);
-
-#endif
