5119808fbe7c1d0b325dacb729c1ed7c3b0d6f39
Alexander Gordeev <a.gordeev.box@gmail.com>
2026-01-11 11:01:10 +0100

    rust: dmaengine bindings
    Signed-off-by: Alexander Gordeev <a.gordeev.box@gmail.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -51,6 +51,7 @@
 #include <linux/device/faux.h>
 #include <linux/dma-direction.h>
 #include <linux/dma-mapping.h>
+#include <linux/dmaengine.h>
 #include <linux/errname.h>
 #include <linux/ethtool.h>
 #include <linux/fdtable.h>
